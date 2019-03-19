bc8704a36c40d0e6b8cab17761cead5092134d1f
Gjorgji Kongulovski <46351282+Hellinho85@users.noreply.github.com>
2019-03-19 06:30:28 +0100

    HW8-Collections
    Collections Ex

---
+++ Homework8/Homework8/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class Person
+    {
+        // Create a class Person that will have 4 properties (FirstName, LastName, Age, Gender-that will be enumeration).
+        // The person class should have a virtual method GetInfo that will print the information of that person.
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public Gender Gender;
+
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"FirstName: {FirstName}, LastName: {LastName}, Age: {Age}, Gender: {Gender}.");
+        }
+
+    }
+}
---
+++ Homework8/Homework8/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    class Student : Person
+    {
+        // Create a class Student that will derive from the Person class. Student class should have properties Id, Group, Course.
+        // Student class should override GetInfo method and should return the Full name of student plus the id and witch group is in.
+        public int Group;
+        public string Course;
+        public int ID;
+
+        public override void GetInfo()
+        {
+            Console.WriteLine($"FullName: {FirstName} {LastName}, Age: {Age}, Gender: {Gender}, ID: {ID}, Group: {Group}.");
+        }
+
+    }
+}
